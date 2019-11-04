f2b01379d4b9a00f0c6e205f8a662609328482f6
Lennart Schoch <lennart.schoch@code.berlin>
2019-11-04 17:24:47 +0100

    Add some test code to make testing on Raspberry Pi easy

--- server.go
+++ server.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"github.com/lennartschoch/vuvuzela/connection"
+	"os"
+)
+
 func main() {
-	// TODO: Start GRPC server
+	// TODO: Remove test code and start GRPC server instead
+
+	if len(os.Args) > 1 {
+
+		arg := os.Args[1]
+		if arg == "send" {
+			config := connection.Configuration{}
+			conn := connection.New(&config)
+			err := conn.Send(65, "testmessage")
+			if err != nil {
+				panic(err)
+			}
+		}
+
+	}
 }
